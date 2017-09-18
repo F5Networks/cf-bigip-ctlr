c6fbc156f9c0e0fae4b9fece57507d6f667cee00
Michael Day <m.day@f5.com>
2017-09-18 15:38:38 -0600

    Add travis CI part 1
    Problem:
    Repo needs CI that tests code, and pushes images to dockerhub.
    
    Solution:
    Added .travis.yml that builds and pushes an image to dockerhub that can be used for system tests and nightly regressions.
    
    Note:
    CI set up is being broken into two stages to simplify moving ongoing development. Created issue #12 for CI part 2.

--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -6,7 +6,16 @@ set -e
 CURDIR="$(dirname $BASH_SOURCE)"
 
 . $CURDIR/_build-lib.sh
+BUILDDIR=$(get_builddir)
+
+export BUILDDIR=$BUILDDIR
 
 go_install $(all_pkgs)
 
 ginkgo_test_with_coverage
+
+# reset GOPATH after using temp directories
+export GOPATH=/build
+
+# run python tests
+./build-tools/python-tests.sh
